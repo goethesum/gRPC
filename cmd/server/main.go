a16ecdcc49c75992da1b3caf27a450697c4e51a9
herman <goethesum@gmail.com>
2022-03-13 22:31:54 +0500

    added sqlx

---
+++ cmd/server/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+
+	"github.com/goethesum/gRPC/internal/db"
+	"github.com/goethesum/gRPC/internal/rocket"
+)
+
+func Run() error {
+	// responsible for initilazing and starting
+	// our gRPC server
+	rocketStore, err := db.New()
+	if err != nil {
+		return err
+	}
+	_ = rocket.New(rocketStore)
+	return nil
+}
+
+func main() {
+	if err := Run(); err != nil {
+		log.Fatal(err)
+	}
+}
